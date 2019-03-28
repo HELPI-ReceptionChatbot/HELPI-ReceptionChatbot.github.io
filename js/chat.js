9d9b491cbd5040bfe94c4453588e081117dfbb00
Mahendrakumar Sharma <mkbls77@gmail.com>
2019-03-28 21:05:05 +0530

    Some Updates

--- js/chat.js
+++ js/chat.js
@@ -8,23 +8,21 @@ function off() {
   document.getElementById("fax").style.display = "block";
 }
 
-function reply() {
-  temp=document.getElementById("in").value;
-  if (temp=="hello") {
-	document.getElementById("in").value = "Welcome";
-  }
-}
-
 
 $(".messages").animate({ scrollTop: $(document).height() }, "fast");
 
 function newMessage() {
   message = $(".message-input input").val();
+  reply = '';
   if($.trim(message) == '') {
     return false;
   }
-  $('<li class="sent"><p>' + message + '</p></li>').appendTo($('.messages ul'));
-  $('<li class="replies"><img src="img/helpi.png" alt="" /><p>' + message + '</p></li>').appendTo($('.messages ul'));
+  if(message.search("hello")!=-1)
+	  reply="Hello!! How are you?";
+  else
+	  reply="Please ask again!!";
+  $('<li class="replies"><p>' + message + '</p></li>').appendTo($('.messages ul'));
+  $('<li class="sent"><img src="img/helpi.png" alt="" /><p>' + reply + '</p></li>').appendTo($('.messages ul'));
   $('.message-input input').val(null);
   $(".messages").animate({ scrollTop: $(document).height() }, "fast");
 };
